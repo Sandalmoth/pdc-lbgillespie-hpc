267e7887217da9a923d5c0d0d5446d0b35435484
Jonathan Lindström <jonathan.lindstrom@lnu.se>
2018-08-29 11:26:39 +0200

    add timer declarations to tracked files

---
+++ code/timers.h
@@ -0,0 +1,33 @@
+#ifndef __TIMERS_H__
+#define __TIMERS_H__
+
+
+#include <chrono>
+
+
+struct IntervalTimer {
+  IntervalTimer() {
+    prev = std::chrono::steady_clock::now();
+  }
+  long long operator()() {
+    auto diff = std::chrono::steady_clock::now() - prev;
+    prev = std::chrono::steady_clock::now();
+    return std::chrono::duration_cast<std::chrono::microseconds>(diff).count();
+  }
+  std::chrono::steady_clock::time_point prev;
+};
+
+
+struct StartTimer {
+  StartTimer() {
+    start = std::chrono::steady_clock::now();
+  }
+  long long operator()() {
+    auto diff = std::chrono::steady_clock::now() - start;
+    return std::chrono::duration_cast<std::chrono::seconds>(diff).count();
+  }
+  std::chrono::steady_clock::time_point start;
+};
+
+
+#endif
